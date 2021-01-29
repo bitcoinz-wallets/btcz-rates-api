50e449310cf1d1be261c811b623fc64071ac06dc
MarcelusCH <buob.marcel@gmail.com>
2021-01-29 22:55:00 +0100

    updated API source

--- src/services/btczRates.js
+++ src/services/btczRates.js
@@ -3,12 +3,12 @@ const request = require('request-promise-native');
 const btczRates = {
   getAll() {
     return Promise.all([
-      request({ uri: 'https://api.coinmarketcap.com/v1/ticker/bitcoinz/', json: true }),
+      request({ uri: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoinz&vs_currencies=btc', json: true }),
       request({ uri: 'https://bitpay.com/api/rates', json: true }),
     ]).then((results) => {
-      const cmcData = results[0]; // results from coinmarketcap
+      //const cmcData = results[0]; // results from coinmarketcap
       const bitpayData = results[1]; // results from bitpay
-      const btczBtcExchangeRate = cmcData[0].price_btc;
+      const btczBtcExchangeRate = results[0].bitcoinz.btc;
       const rates = [];
 
       bitpayData.forEach((value) => {
